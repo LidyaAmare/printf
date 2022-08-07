facad7d681064ec96fa9a71f2698765dc1b6d6ec
Ismael Mohammed <ismaelmohammed97@gmail.com>
2022-08-07 16:36:48 +0300

    A function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -9,42 +9,36 @@
  */
 int _printf(const char *format, ...)
 {
-int (*pfunc)(va_list, flags_t *);
-const char *p;
-va_list arguments;
-flags_t flags = {0, 0, 0};
-register int count = 0;
-va_start(arguments, format);
-if (!format || (format[0] == '%' && !format[1]))
-return (-1);
-if (format[0] == '%' && format[1] == ' ' && !format[2])
-return (-1);
-for (p = format; *p; p++)
-{
-if (*p == '%')
-{
-p++;
-if (*p == '%')
-{
-count += _putchar('%');
-continue;
+	int check = 0, i;
+	va_list parameters;
+	int (*func)(va_list);
 
-}
-while (get_flag(*p, &flags))
-p++;
-pfunc = get_print(*p);
-count += (pfunc)
-? pfunc(arguments, &flags)
-: _printf("%%%c", *p);
-
-}
-else
-count += _putchar(*p);
-
-}
-
-_putchar(-1);
-va_end(arguments);
-return (count);
-
+	va_start(parameters, format);
+	if (format == NULL)
+		return (-1);
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (!(format[i]))
+				return (-1);
+			func = get_func(format[i]);
+			if (func == NULL)
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				check += 2;
+			}
+			else
+				check += func(parameters);
+		}
+		else
+		{
+			_putchar(format[i]);
+			check++;
+		}
+	}
+	va_end(parameters);
+	return (check);
 }
