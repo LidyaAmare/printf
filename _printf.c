97acc714ba36f5c7556e200c801a92d3b82f717d
Ismael Mohammed <ismaelmohammed97@gmail.com>
2022-08-07 18:03:14 +0300

    A function that produces output according to a format

--- _printf.c
+++ _printf.c
@@ -7,44 +7,59 @@
  * specifiers contained into fmt
  * Return: length of the formatted output string
  */
-nt _printf(const char *format, ...)
+int op_checker(char c, va_list ap)
 {
-	va_list args;
-	int i = 0, chars_printed = 0;
+	int i;
+	func_t ops[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"d", print_int},
+		{"i", print_int},
+		{"%", print_percent},
+		{"b", print_binary},
+		{NULL, NULL}
+	};
 
-	va_start(args, format);
-	while (format && format[i])
+	for (i = 0; ops[i].op != NULL; i++)
 	{
-		if (format[i] != '%')
+		if (c == *(ops[i].op))
 		{
-			chars_printed += _putchar(format[i]);
+			return (ops[i].f(ap));
 		}
-		else if (format[i + 1])
+	}
+	return (-1);
+}
+
+/**
+ * _printf - Printf function
+ * @format: The format of the string
+ *
+ * Return: The number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i = 0, ret = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(ap, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == 'c' || format[i] == 's')
-				chars_printed += format[i] == 'c' ? _putchar(va_arg(args, int)) :
-				print_string(va_arg(args, char *));
-			else if (format[i] == 'd' || format[i] == 'i')
-				chars_printed += print_num(va_arg(args, int));
-			else if (format[i] == 'b')
-				chars_printed += print_binary((unsigned int)va_arg(args, int));
-			else if (format[i] == 'r')
-				chars_printed += print_reverse(va_arg(args, char *));
-			else if (format[i] == 'R')
-				chars_printed += print_rot13(va_arg(args, char *));
-			else if (format[i] == 'o' || format[i] == 'u' ||
-			format[i] == 'x' || format[i] == 'X')
-				chars_printed += print_odh(format[i], (unsigned int)va_arg(args, int));
-			else if (format[i] == 'S')
-				chars_printed += print_S(va_arg(args, char *));
-			else if (format[i] == 'p')
-				chars_printed += print_pointer(va_arg(args, void *));
-			else
-				chars_printed += print_unknown_spec(format[i]);
+			ret += op_checker(format[i], ap);
+		}
+		else
+		{
+			_putchar(format[i]);
+			ret++;
 		}
 		i++;
 	}
-	va_end(args);
-	return (chars_printed);
+	va_end(ap);
+
+	return (ret);
 }
